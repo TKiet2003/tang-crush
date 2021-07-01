0e38b5f37a51deecf07b36cba20eb05299a4aa95
TKiet2003 <86753945+TKiet2003@users.noreply.github.com>
2021-07-01 10:25:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe Nhii à !',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Ê Kim Thanh !',
+    introDesc: `Nói nghe nè`,
+    btnIntro: 'Bấm Dô đây nè',
+    title: 'Tao đói quá à',
+    desc: 'Trả cái lẩu đi chứ bạn êi ',
+    btnYes: 'Buộc nấu rồi 🤣😂',
+    btnNo: 'Không nấu đâu nha 😜',
+    question:'Trên thế giới hơn 7 tỉ người sao có mình m thiếu nợ ',
+    btnReply: 'gửi cho Kiệt ',
+    reply: 'Nấu thì nấu ko nấu thì nấu <33333333',
+    mess: 'Kiệt biết mà 🥰. Yêu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h tao qua ăn nheee.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
